f4b69c7de9667105856f24278e381848aa88ac7e
Andreas Pätzold <andi.paetzold@gmail.com>
2017-11-30 00:05:18 +0100

    Map uses full space and overlay can be shown by clicking a button

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ import { fetchToken, getAuthCode } from "./strava-auth.js";
 import { getParameterByName } from "./util.js";
 import { initMap } from "./map.js";
 
-let map;
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kaXBhZXR6b2xkIiwiYSI6ImNqOWgyY2F5NjBnNnAyeXBodzByemRsbWoifQ.wW4aCiUFv2PLhGB2S75sNg';
 
-
 document.addEventListener('DOMContentLoaded', async () => {
+    initMap();
+
     if (getAuthCode()) {
         document.getElementById('auth').parentNode.removeChild(document.getElementById('auth'));
-
-        initMap();
 
         const loader = new ActivityLoader();
         await loader.loadActivities(await fetchToken());
     }
 });
+
+window.toggleOverlay = () => {
+    const overlay = document.getElementById('overlay');
+    const toggle = document.getElementById('overlay-toggle');
+    if (overlay.classList.contains('visible')) {
+        overlay.classList.remove('visible');
+        toggle.classList.remove('btn-primary');
+    } else {
+        overlay.classList.add('visible');
+        toggle.classList.add('btn-primary');
+    }
+}
