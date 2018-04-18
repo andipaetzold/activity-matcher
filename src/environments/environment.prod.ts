960b5e928d28ad99eb8627aec8badd4a56a07067
Andi Pätzold <andi.paetzold@gmail.com>
2018-04-18 18:54:07 +0200

    Fixed deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,20 @@ import { FirebaseAppConfig } from 'angularfire2';
 
 export const environment = {
     production: true,
+    firebase: <FirebaseAppConfig>{
+        apiKey: 'AIzaSyDHCdOGIDCKuTAfGjjqqRMczFzpBQF3C',
+        storageBucket: 'activity-matcher.appspot.com',
+        projectId: 'activity-matcher',
+    },
     strava: {
-        redirectUri: 'https://activity-matcher.andipaetzold.com/'
-    }
+        clientId: '21035',
+        clientSecret: 'e0006d0074d5628466f4aca55b6e83ba4fe4b9df',
+        redirectUri: 'https://localhost:4200'
+    },
+    mapbox: {
+        accessToken: 'pk.eyJ1IjoiYW5kaXBhZXR6b2xkIiwiYSI6ImNqOWgyY2F5NjBnNnAyeXBodzByemRsbWoifQ.wW4aCiUFv2PLhGB2S75sNg',
+    },
+    googleMaps: {
+        key: 'AIzaSyB-fdWUHhDXoXikdPm5puhS0QD-j6HBWSo',
+    },
 };
