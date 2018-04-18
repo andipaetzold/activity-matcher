c7b449214c7d2fc6050007d7e44e5e27226b5658
Andi Pätzold <andi.paetzold@gmail.com>
2018-04-18 20:29:48 +0200

    Transit icons are also hidden

--- src/app/components/map-google/map-google.component.ts
+++ src/app/components/map-google/map-google.component.ts
@@ -33,6 +33,11 @@ export class MapGoogleComponent implements OnInit {
                     featureType: 'poi',
                     stylers: [{ visibility: 'off' }]
                 },
+                {
+                    featureType: 'transit',
+                    elementType: 'labels.icon',
+                    stylers: [{ visibility: 'off' }]
+                }
             ]
         });
     }
